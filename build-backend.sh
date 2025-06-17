fe4c6e671dd5135e1e42eefbc56afc63dca8028f
Žygimantas Arūna <zygimantas.aruna@roxit.nl>
2025-06-17 09:42:50 +0300

    Drop mac build for now

--- build-backend.sh
+++ build-backend.sh
@@ -1,4 +1,3 @@
 dotnet restore ./backend/ClefViewer/ClefViewer.API/ClefViewer.API.csproj
 dotnet publish ./backend/ClefViewer/ClefViewer.API/ClefViewer.API.csproj --no-restore --runtime linux-x64 --output ./publish/linux --self-contained -p:PublishTrimmed=true
-dotnet publish ./backend/ClefViewer/ClefViewer.API/ClefViewer.API.csproj --no-restore --runtime osx-x64 --output ./publish/osx --self-contained -p:PublishTrimmed=true
 dotnet publish ./backend/ClefViewer/ClefViewer.API/ClefViewer.API.csproj --no-restore --runtime win-x64 --output ./publish/win --self-contained -p:PublishTrimmed=true
